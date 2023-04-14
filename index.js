198854f25636e12dc2790a65a3aa5ff91e02af1d
Achieve-software <vtcbcdtyr@gmail.com>
2023-04-14 20:47:48 +0300

    numberofdigits

---
+++ index.js
@@ -0,0 +1,9 @@
+//Console'dan girilen bir sayinin basamak sayisini hesaplayarak console'a
+//bastiran kodu donguler yardimiyle yaziniz.
+let counter = 0;
+let number = +prompt("your number: ");
+while (number != 0) {
+  number = Math.trunc(number / 10);
+  counter++;
+}
+console.log("digit count is " + counter);
